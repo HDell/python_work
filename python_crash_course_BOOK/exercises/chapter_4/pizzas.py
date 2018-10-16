793f333fa0bf4f82c4dee60c90c7ee0bc8f376a0
Handell Desulme <handelldesulme@8c-85-90-bc-eb-12.wireless.temple.edu>
2018-10-15 20:00:32 -0400

    Adds Chapter 4 exercices (pizza.py complete)

---
+++ python_crash_course_BOOK/exercises/chapter_4/pizzas.py
@@ -0,0 +1,10 @@
+
+
+#initializations
+pizzas = ["pepporoni", "cheese", "bacon"];
+
+if __name__ == '__main__':
+	for pizza in pizzas:
+		print(pizza);
+		print("I like", pizza, "pizza.");
+	print("Seems like I really like pizza.");
